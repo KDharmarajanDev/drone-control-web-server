492b458134fef0dd038b4f55c83a16f541eb8462
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-12-19 11:58:21 -0800

    Added arm endpoint

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const rosnodejs = require('rosnodejs');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + "/html/index.html");
+});
+
+app.post('/api/arm/:arm_status?', (req, res) => {
+    console.log(req.params);
+    if (req.params.arm_status == "true"){
+        
+    } else if (req.params.arm_status == "false"){
+
+    }
 });
 
 app.listen(port, () => {
