1c24e925b42dba14b49e016b9b158eef8ed79bbc
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-01-12 23:01:02 -0800

    Reorganized video.js

--- devel/js/video.js
+++ devel/js/video.js
@@ -1,27 +1,19 @@
 const io = require('socket.io-client');
 var socket = io();
-socket.on('video', (data) => {
-    const image = document.getElementById('video-feed');
-    image.src = `data:image/jpeg;base64,${data}`;
-});
-
-// Handles connections
-if (socket.connected) {
-    document.getElementById('connection-status').innerHTML = "Connected";
-    document.getElementById('connection-status').style.color = "green";
-}
 
 socket.on('connection', () =>{
     document.getElementById('connection-status').innerHTML = "Connected";
     document.getElementById('connection-status').style.color = "green";
     console.log('Connected!');
+    socket.on('disconnect', () =>{
+        document.getElementById('connection-status').innerHTML = "Disconnected";
+        document.getElementById('connection-status').style.color = "red";
+        console.log('Disconnected!');
+    });
+    socket.on('video', (data) => {
+        const image = document.getElementById('video-feed');
+        image.src = `data:image/jpeg;base64,${data}`;
+    });
 });
-
-socket.on('disconnection', () =>{
-    document.getElementById('connection-status').innerHTML = "Disconnected";
-    document.getElementById('connection-status').style.color = "red";
-    console.log('Disconnected!');
-});
-
 
 export {socket}
