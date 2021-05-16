3cd68231621f51045ffce7db13bb656e177ec45f
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-05-15 19:36:15 -0700

    Futhered data container

---
+++ devel/js/data-visualizer.js
@@ -0,0 +1,8 @@
+class PlotContainerTile {
+    constructor() {
+        // Creates the overall parent container 
+        this.parentDiv = document.createElement('div');
+        this.parentDiv.classList.add('dashboard-section');
+
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: ['/devel/js/video.js', '/devel/js/visualization.js', '/devel/js/document-events.js'],
+    entry: ['/devel/js/video.js', '/devel/js/visualization.js', '/devel/js/document-events.js', '/devel/js/data-visualizer.js'],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'public/js')
