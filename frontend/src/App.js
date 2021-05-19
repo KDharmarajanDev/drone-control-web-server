b64e72956a1f08c788c3b0777a2bac99e6decd34
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-05-18 21:54:53 -0700

    Added app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div id="background">
+    <div id="main-content">
+      <h1 id="main-title">Drone Control Web Server</h1>
+      <div>
+        <p id="connection-status">Disconnected</p>
+      </div>
+      <button id="arm-button" class="action-button">Arm</button>
+      <button id="fly-button" class="action-button">Fly Home</button>
+      <button id="test" class="action-button">Test</button>
+      <button id="pause-mission-button" class="action-button">Pause Mission/Rest</button>
+      <div class="booth">
+        <img id="video-feed"/>
+      </div>
+      <div id="map">
+      </div>
+      <h2 id="data-plotting-title">Data Plotting</h2>
+      <div id="data" class="dashboard-section">
+        <input type="text" class="dashboard-section-title" size="20" placeholder="Data Plot Title"/>
+        <div class="topic-data-container">
+          <label for="data-topic-input" class="input-box-labels">ROS Topic of Data:</label>
+          <input type="text" id="data-topic-input" size="13"/>
+        </div>
+        <div class="topic-data-container">
+          <label for="data-topic-input" class="input-box-labels">Data Being Plotted:</label>
+          <input type="text" size="13"/>
+        </div>
+        <button id="start-plotting-button">Start Plotting Data</button>
+        <div id="plot" class="data-plots"></div>
+      </div>
+      <script type="module" src="/public/js/main.js"></script> 
     </div>
+  </div>
   );
 }
 
