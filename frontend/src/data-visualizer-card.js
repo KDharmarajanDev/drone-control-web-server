ab2d2d1d4a4c59cf1ba095f8cf00daa99b5bcf59
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-07-13 23:18:56 -0700

    Added data updating indicator

--- frontend/src/data-visualizer-card.js
+++ frontend/src/data-visualizer-card.js
@@ -3,7 +3,10 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import { makeStyles } from '@material-ui/core/styles';
 import { LineChart } from 'recharts';
-import { Button } from '@material-ui/core';
+import { Button, Avatar } from '@material-ui/core';
+import { useState } from 'react';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 const useStyles = makeStyles({
     root: {
@@ -13,6 +16,12 @@ const useStyles = makeStyles({
     title: {
         color: "black",
         fontFamily: ["Duru Sans", "sans-serif"]
+    },
+    updatingCard: {
+        backgroundColor: "green"
+    },
+    notUpdatingCard: {
+        backgroundColor: "red"
     }
 });
   
@@ -20,9 +29,20 @@ export default function DataVizCard(props) {
     const classes = useStyles();
 
     const data = [];
+    const [isUpdating, setIsUpdating] = useState(false);
 
     return (<Card className={classes.root} key={props.dataBox.name}>
-                <CardHeader title={props.dataBox.name} className={classes.title}/>
+                <CardHeader 
+                avatar = {
+                    isUpdating ? 
+                    (<Avatar className={classes.updatingCard}>
+                        <CheckCircleOutlineIcon/>
+                    </Avatar>) : 
+                    (<Avatar className={classes.notUpdatingCard}>
+                        <HighlightOffIcon/>
+                    </Avatar>)
+                }
+                title={props.dataBox.name} className={classes.title}/>
                 <LineChart width={200} height={200} data={data}>
                 </LineChart>
                 <Button></Button>
